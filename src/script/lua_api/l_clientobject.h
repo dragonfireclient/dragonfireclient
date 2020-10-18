7ed22368606b49bff31f60d2eaadbb96106ea85b
Elias Fleckenstein <eliasfleckenstein@web.de>
2020-10-18 14:21:24 +0200

    Replaced spaces with tabs

--- src/script/lua_api/l_clientobject.h
+++ src/script/lua_api/l_clientobject.h
@@ -24,54 +24,54 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 class ClientObjectRef : public ModApiBase
 {
 public:
-    ClientObjectRef(ClientActiveObject *object);
+	ClientObjectRef(ClientActiveObject *object);
 
-    ~ClientObjectRef() = default;
+	~ClientObjectRef() = default;
 
-    static void Register(lua_State *L);
+	static void Register(lua_State *L);
 
-    static void create(lua_State *L, ClientActiveObject *object);
+	static void create(lua_State *L, ClientActiveObject *object);
 
-    static ClientObjectRef *checkobject(lua_State *L, int narg);
+	static ClientObjectRef *checkobject(lua_State *L, int narg);
 
 private:
-    ClientActiveObject *m_object = nullptr;
-    static const char className[];
-    static luaL_Reg methods[];
+	ClientActiveObject *m_object = nullptr;
+	static const char className[];
+	static luaL_Reg methods[];
 
-    static ClientActiveObject *get_cao(ClientObjectRef *ref);
-    static GenericCAO *get_generic_cao(ClientObjectRef *ref, lua_State *L);
+	static ClientActiveObject *get_cao(ClientObjectRef *ref);
+	static GenericCAO *get_generic_cao(ClientObjectRef *ref, lua_State *L);
 
-    static int gc_object(lua_State *L);
+	static int gc_object(lua_State *L);
 
-    // get_pos(self)
-    // returns: {x=num, y=num, z=num}
-    static int l_get_pos(lua_State *L);
+	// get_pos(self)
+	// returns: {x=num, y=num, z=num}
+	static int l_get_pos(lua_State *L);
 
-    // get_velocity(self)
-    static int l_get_velocity(lua_State *L);
+	// get_velocity(self)
+	static int l_get_velocity(lua_State *L);
 
-    // get_acceleration(self)
-    static int l_get_acceleration(lua_State *L);
+	// get_acceleration(self)
+	static int l_get_acceleration(lua_State *L);
 
-    // get_rotation(self)
-    static int l_get_rotation(lua_State *L);
+	// get_rotation(self)
+	static int l_get_rotation(lua_State *L);
 
-    // is_player(self)
-    static int l_is_player(lua_State *L);
+	// is_player(self)
+	static int l_is_player(lua_State *L);
 
-    // get_name(self)
-    static int l_get_name(lua_State *L);
+	// get_name(self)
+	static int l_get_name(lua_State *L);
 
-    // get_attach(self)
-    static int l_get_attach(lua_State *L);
+	// get_attach(self)
+	static int l_get_attach(lua_State *L);
 
-    // get_nametag(self)
-    static int l_get_nametag(lua_State *L);
+	// get_nametag(self)
+	static int l_get_nametag(lua_State *L);
 
-    // get_textures(self)
-    static int l_get_item_textures(lua_State *L);
+	// get_textures(self)
+	static int l_get_item_textures(lua_State *L);
 
-    // get_hp(self)
-    static int l_get_max_hp(lua_State *L);
+	// get_hp(self)
+	static int l_get_max_hp(lua_State *L);
 };
