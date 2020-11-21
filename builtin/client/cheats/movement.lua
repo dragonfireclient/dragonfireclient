5fcac97729aa0b3d5f06ec9daa6bf32867425c2e
cora <email@example.com>
2020-11-21 12:38:31 +0100

    movement: add autosprint and autosneak_conditional

--- builtin/client/cheats/movement.lua
+++ builtin/client/cheats/movement.lua
@@ -1,6 +1,8 @@
 -- autosneak
 
 local autosneak_was_enabled = false
+local autosneak_conditional_was_enabled = false
+local autosprint was enabled
 
 core.register_globalstep(function()
 	if core.settings:get_bool("autosneak") then
@@ -10,5 +12,24 @@ core.register_globalstep(function()
 		autosneak_was_enabled = false
 		core.set_keypress("sneak", false)
 	end
+
+        if minetest.settings:get_bool("autosneak_conditional") then
+		if minetest.localplayer:is_touching_ground() then
+			minetest.set_keypress("sneak", true)
+			autosprint_conditional_was_enabled = true
+		end
+	elseif autosprint_conditional_was_enabled then
+		autosprint_conditional_was_enabled = false
+		minetest.set_keypress("sneak", false)
+	end
+
+    	if minetest.settings:get_bool("autosprint") or (minetest.settings:get_bool("continuous_forward") and minetest.settings:get_bool("autofsprint")) then
+        	core.set_keypress("special1", true)
+        	sprint_was_enabled = true
+	elseif sprint_was_enabled then
+        	core.set_keypress("special1", false)
+        	sprint_was_enabled = false
+    	end
+
 end)
  
