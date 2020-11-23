f078f2e3d25c81b0882c76909d3864417f081052
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2020-11-23 16:10:33 +0000

    Add PlayerESP

--- src/client/render/core.h
+++ src/client/render/core.h
@@ -40,6 +40,7 @@ protected:
 	bool draw_tracers;
 	bool draw_node_esp;
 	bool draw_node_tracers;
+	bool draw_player_esp;
 
 	IrrlichtDevice *device;
 	video::IVideoDriver *driver;
@@ -75,7 +76,8 @@ public:
 	void initialize();
 	void draw(video::SColor _skycolor, bool _show_hud, bool _show_minimap,
 			bool _draw_wield_tool, bool _draw_crosshair, bool _draw_esp,
-			bool _draw_tracers, bool _draw_node_esp, bool _draw_node_tracers);
+			bool _draw_tracers, bool _draw_node_esp, bool _draw_node_tracers,
+		 	bool _draw_player_esp);
 
 	inline v2u32 getVirtualSize() const { return virtual_size; }
 };
