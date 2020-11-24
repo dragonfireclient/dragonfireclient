9c4d42acc22631a9f8168b0d8211581aeff29b50
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2020-11-24 15:39:27 +0000

    Remove OnlyTracePlayers. Add EntityESP and EntityTracers

--- src/client/render/core.cpp
+++ src/client/render/core.cpp
@@ -57,7 +57,7 @@ void RenderingCore::updateScreenSize()
 }
 
 void RenderingCore::draw(video::SColor _skycolor, bool _show_hud, bool _show_minimap,
-		bool _draw_wield_tool, bool _draw_crosshair, bool _draw_esp, bool _draw_tracers, bool _draw_node_esp, bool _draw_node_tracers)
+		bool _draw_wield_tool, bool _draw_crosshair, bool _draw_esp, bool _draw_tracers, bool _draw_node_esp, bool _draw_node_tracers, bool _draw_entity_esp, bool _draw_entity_tracers)
 {
 	v2u32 ss = driver->getScreenSize();
 	if (screensize != ss) {
@@ -73,6 +73,8 @@ void RenderingCore::draw(video::SColor _skycolor, bool _show_hud, bool _show_min
 	draw_tracers = _draw_tracers;
 	draw_node_esp = _draw_node_esp;
 	draw_node_tracers = _draw_node_tracers;
+	draw_entity_esp = _draw_entity_esp;
+	draw_entity_tracers = _draw_entity_tracers;
 		
 	beforeDraw();
 	drawAll();
@@ -80,7 +82,6 @@ void RenderingCore::draw(video::SColor _skycolor, bool _show_hud, bool _show_min
 
 void RenderingCore::drawTracersAndESP()
 {
-	bool only_trace_players = g_settings->getBool("only_trace_players");
 	
 	ClientEnvironment &env = client->getEnv();
 	Camera *camera = client->getCamera();
@@ -107,18 +108,66 @@ void RenderingCore::drawTracersAndESP()
 			GenericCAO *obj = dynamic_cast<GenericCAO *>(cao);
 			if (! obj)
 				continue;
-			if (only_trace_players && ! obj->isPlayer())
-				continue;
 			aabb3f box;
 			if (! obj->getSelectionBox(&box))
+				continue;
+			if (! obj->isPlayer())
 				continue;
 			v3f pos = obj->getPosition() - camera_offset;
 			box.MinEdge += pos;
 			box.MaxEdge += pos;
+			
+			auto esp_color = g_settings->get("esp_color");
+			auto tracers_color = g_settings->get("tracers_color");
+			
 			if (draw_esp)
-				driver->draw3DBox(box, video::SColor(255, 255, 255, 255));
+				if (m_esp_color == "red")
+					driver->draw3DBox(box, video::SColor(0xFF0000));
+				else if (m_esp_color == "lime")
+					driver->draw3DBox(box, video::SColor(0x00FF00));
+				else if (m_esp_color == "purple")
+					driver->draw3DBox(box, video::SColor(0x800080));
+				else if (m_esp_color == "pink")
+					driver->draw3DBox(box, video::SColor(0xFFC0CB));
+				else if (m_esp_color == "green")
+					driver->draw3DBox(box, video::SColor(0x008000));
+				else if (m_esp_color == "blue")
+					driver->draw3DBox(box, video::SColor(0x0000FF));
+				else if (m_esp_color == "yellow")
+					driver->draw3DBox(box, video::SColor(0xFFFF00));
+				else if (m_esp_color == "white")
+					driver->draw3DBox(box, video::SColor(0xFFFFFF));
+				else if (m_esp_color == "black")
+					driver->draw3DBox(box, video::SColor(0x000000));
+				else if (m_esp_color == "brown")
+					driver->draw3DBox(box, video::SColor(0xA52A2A));
+				else if (m_esp_color == "orange")
+					driver->draw3DBox(box, video::SColor(0xFFA500));
+				else
+					driver->draw3DBox(box, video::SColor(0xFFFFFF));
 			if (draw_tracers)
-				driver->draw3DLine(eye_pos, box.getCenter(), video::SColor(255, 255, 255, 255));
+				if (m_tracers_color == "red")
+					driver->draw3DBox(box, video::SColor(0xFF0000));
+				else if (m_tracers_color == "lime")
+					driver->draw3DBox(box, video::SColor(0x00FF00));
+				else if (m_tracers_color == "purple")
+					driver->draw3DBox(box, video::SColor(0x800080));
+				else if (m_tracers_color == "pink")
+					driver->draw3DBox(box, video::SColor(0xFFC0CB));
+				else if (m_tracers_color == "green")
+					driver->draw3DBox(box, video::SColor(0x008000));
+				else if (m_tracers_color == "blue")
+					driver->draw3DBox(box, video::SColor(0x0000FF));
+				else if (m_tracers_color == "yellow")
+					driver->draw3DBox(box, video::SColor(0xFFFF00));
+				else if (m_tracers_color == "white")
+					driver->draw3DBox(box, video::SColor(0xFFFFFF));
+				else if (m_tracers_color == "black")
+					driver->draw3DBox(box, video::SColor(0x000000));
+				else if (m_tracers_color == "brown")
+					driver->draw3DBox(box, video::SColor(0xA52A2A));
+				else if (m_tracers_color == "orange")
+					driver->draw3DBox(box, video::SColor(0xFFA500));
 		}
 	}
 	if (draw_node_esp || draw_node_tracers) {
@@ -151,6 +200,79 @@ void RenderingCore::drawTracersAndESP()
 			}
 		}
 
+	}
+	
+	if (draw_entity_esp || draw_entity_tracers) {
+		auto allObjects = env.getAllActiveObjects();
+
+		for (auto &it : allObjects) {
+			ClientActiveObject *cao = it.second;
+			if (cao->isLocalPlayer() || cao->getParent())
+				continue;
+			GenericCAO *obj = dynamic_cast<GenericCAO *>(cao);
+			if (! obj)
+				continue;
+			aabb3f box;
+			if (! obj->getSelectionBox(&box))
+				continue;
+			if (obj->isPlayer())
+				continue;
+			v3f pos = obj->getPosition() - camera_offset;
+			box.MinEdge += pos;
+			box.MaxEdge += pos;
+			
+			auto esp_color = g_settings->get("entity_esp_color");
+			auto tracers_color = g_settings->get("entity_tracers_color");
+			
+			if (draw_esp)
+				if (m_esp_color == "red")
+					driver->draw3DBox(box, video::SColor(0xFF0000));
+				else if (m_esp_color == "lime")
+					driver->draw3DBox(box, video::SColor(0x00FF00));
+				else if (m_esp_color == "purple")
+					driver->draw3DBox(box, video::SColor(0x800080));
+				else if (m_esp_color == "pink")
+					driver->draw3DBox(box, video::SColor(0xFFC0CB));
+				else if (m_esp_color == "green")
+					driver->draw3DBox(box, video::SColor(0x008000));
+				else if (m_esp_color == "blue")
+					driver->draw3DBox(box, video::SColor(0x0000FF));
+				else if (m_esp_color == "yellow")
+					driver->draw3DBox(box, video::SColor(0xFFFF00));
+				else if (m_esp_color == "white")
+					driver->draw3DBox(box, video::SColor(0xFFFFFF));
+				else if (m_esp_color == "black")
+					driver->draw3DBox(box, video::SColor(0x000000));
+				else if (m_esp_color == "brown")
+					driver->draw3DBox(box, video::SColor(0xA52A2A));
+				else if (m_esp_color == "orange")
+					driver->draw3DBox(box, video::SColor(0xFFA500));
+				else
+					driver->draw3DBox(box, video::SColor(0xFFFFFF));
+			if (draw_tracers)
+				if (m_tracers_color == "red")
+					driver->draw3DBox(box, video::SColor(0xFF0000));
+				else if (m_tracers_color == "lime")
+					driver->draw3DBox(box, video::SColor(0x00FF00));
+				else if (m_tracers_color == "purple")
+					driver->draw3DBox(box, video::SColor(0x800080));
+				else if (m_tracers_color == "pink")
+					driver->draw3DBox(box, video::SColor(0xFFC0CB));
+				else if (m_tracers_color == "green")
+					driver->draw3DBox(box, video::SColor(0x008000));
+				else if (m_tracers_color == "blue")
+					driver->draw3DBox(box, video::SColor(0x0000FF));
+				else if (m_tracers_color == "yellow")
+					driver->draw3DBox(box, video::SColor(0xFFFF00));
+				else if (m_tracers_color == "white")
+					driver->draw3DBox(box, video::SColor(0xFFFFFF));
+				else if (m_tracers_color == "black")
+					driver->draw3DBox(box, video::SColor(0x000000));
+				else if (m_tracers_color == "brown")
+					driver->draw3DBox(box, video::SColor(0xA52A2A));
+				else if (m_tracers_color == "orange")
+					driver->draw3DBox(box, video::SColor(0xFFA500));
+		}
 	}
 	
 	driver->setMaterial(oldmaterial);
