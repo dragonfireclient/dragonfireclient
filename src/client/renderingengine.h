c15b3c726b86afdbbc9666c5a39d7373793cb1ca
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2020-11-24 15:43:00 +0000

    Add EntityESP and EntityTracers

--- src/client/renderingengine.h
+++ src/client/renderingengine.h
@@ -117,10 +117,10 @@ public:
 	}
 
 	inline static void draw_scene(video::SColor skycolor, bool show_hud,
-			bool show_minimap, bool draw_wield_tool, bool draw_crosshair, bool draw_esp, bool draw_tracers, bool draw_node_esp, bool draw_node_tracers)
+			bool show_minimap, bool draw_wield_tool, bool draw_crosshair, bool draw_esp, bool draw_tracers, bool draw_node_esp, bool draw_node_tracers, bool draw_entity_esp, bool draw_entity_tracers)
 	{
 		s_singleton->_draw_scene(skycolor, show_hud, show_minimap,
-				draw_wield_tool, draw_crosshair, draw_esp, draw_tracers, draw_node_esp, draw_node_tracers);
+				draw_wield_tool, draw_crosshair, draw_esp, draw_tracers, draw_node_esp, draw_node_tracers, bool draw_entity_esp, bool draw_entity_tracers);
 	}
 
 	inline static void initialize(Client *client, Hud *hud)
